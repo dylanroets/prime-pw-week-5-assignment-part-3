7c52b9ebe303f8f1548f585f52948945cf131c25
Dylan Roets <dylanroets@yahoo.com>
2022-10-22 21:19:42 -0500

    fixed messy syntax, forgot ;

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,11 +13,11 @@ function addToCollection(title, artist, yearPublished){
 }
 addToCollection('London Calling', 'The Clash', 1979);
 //adding 6 albums to test
-addToCollection('Sandinista!', 'The Clash', 1980)
-addToCollection('Trompe Le Monde', 'Pixies', 1991)
-addToCollection('The Chronic', 'Dr. Dre', 1992)
-addToCollection('Kind of Blue', 'Miles David', 1959)
-addToCollection('Discovery', 'Daft Punk', 2001)
-addToCollection('Random Access Memories', 'Daft Punk', 2013)
+addToCollection('Sandinista!', 'The Clash', 1980);
+addToCollection('Trompe Le Monde', 'Pixies', 1991);
+addToCollection('The Chronic', 'Dr. Dre', 1992);
+addToCollection('Kind of Blue', 'Miles David', 1959);
+addToCollection('Discovery', 'Daft Punk', 2001);
+addToCollection('Random Access Memories', 'Daft Punk', 2013);
 //testing by displaying current collection, should be 7 total
 console.log(collection);
