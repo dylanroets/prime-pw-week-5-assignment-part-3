9a9659f92eac62aebc78bb0b7bf300e1abda25d5
Dylan Roets <dylanroets@yahoo.com>
2022-10-22 20:36:43 -0500

    Started the function to addtocollection, also declared first record object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,13 @@
 console.log('***** Music Collection *****')
-//start a collection variable
+//start a collection variable and record objects
 let collection = [];
+let record0 = {
+    title: 'London Calling',
+    artist: 'The Clash',
+    yearPublished: 1979
+}
+//creating function to add record to collection
+function addToCollection( title, artist, yearPublished ){
+    console.log('in addToCollection', title, artist, yearPublished)
+
+}
