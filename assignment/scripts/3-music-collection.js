f5e0ce06af5b23fc05bbac3247180fd04e785b16
Dylan Roets <dylanroets@yahoo.com>
2022-10-22 21:03:13 -0500

    finished the function, still unsure what return does and if I need to return newRecord at the end

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,14 @@
 console.log('***** Music Collection *****')
 //start a collection variable and record objects
 let collection = [];
-let record0 = {
-    title: 'London Calling',
-    artist: 'The Clash',
-    yearPublished: 1979
-}
 //creating function to add record to collection
-function addToCollection( title, artist, yearPublished ){
-    console.log('in addToCollection', title, artist, yearPublished)
-
+function addToCollection(title, artist, yearPublished){
+    newRecord = { 
+        title,
+        artist,
+        yearPublished
+    };
+    collection.push(newRecord);
+    return newRecord;
 }
+addToCollection('London Calling', 'The Clash', 1979);
